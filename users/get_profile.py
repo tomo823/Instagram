ab39dc004262c4eeb98241f731ee2fada532701d
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-09-14 20:38:49 +0900

    modified:   get_profile.py

--- users/get_profile.py
+++ users/get_profile.py
@@ -1,33 +1,68 @@
 #latest selenium version is 4.11.2
 #using selenium==3.141.0
+#urllib3==1.24.2
 #Succeeded to login
 
-import time
+import time, pyautogui, csv, os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import csv
-import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 
 #browser Settings
-driver = webdriver.Chrome(executable_path="C:\\Users\\xfura\\Desktop\\Instagram\\Instagram\\login\\chromedriver.exe")
-#opne instagram
+driver = webdriver.Chrome(executable_path=os.path.join("..", "chromedriver.exe"))
+#open instagram
+driver.maximize_window()
 driver.get("https://www.instagram.com")
+
 
 #User settings
 load_dotenv()
 username = os.getenv("MAIL")
 password = os.getenv("PASSWORD")
 
-time.sleep(0.4)
+user_list = []
+with open('followers.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+        user_list.append(row)
+
+#print(user_list[0])
+
+time.sleep(1)
 
 #Login
 loginForm = driver.find_element_by_id("loginForm")
 loginForm.find_element_by_name("username").send_keys(username)
 loginForm.find_element_by_name("password").send_keys(password)
 loginButton = driver.find_element_by_css_selector("button[type=submit]")
+time.sleep(0.4)
 loginButton.click()
 
-time.sleep(4)
+
+"""time.sleep(4)
+#Delete pop-up for Notification
+pyautogui.moveTo(839, 240, duration=0.25)
+pyautogui.click()"""
+
+time.sleep(5)
+"""
+#Delete pop-up for automatic login
+pyautogui.moveTo(1671, 121, duration=0.25)
+pyautogui.click()"""
+
+time.sleep(0.4)
+
+pyautogui.moveTo(100, 300, duration=0.25)
+pyautogui.click()
+
+pyautogui.write(user_list[0][0])
+
+pyautogui.move(0, 50, duration=0.25)
+time.sleep(0.4)
+pyautogui.click()
+
+#print(pyautogui.size())
