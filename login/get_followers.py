df071447f0639486e496a0390c2b91594724b51a
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-10-10 16:57:11 +0900

    deleted:    followers.csv 	modified:   get_followers.py

--- login/get_followers.py
+++ login/get_followers.py
@@ -2,19 +2,28 @@
 #using selenium==3.141.0
 #Succeeded to login
 
-import time
+import time, csv, os, pathlib, sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import csv
-import os
 from dotenv import load_dotenv
+import pyautogui
 
+current_dir = pathlib.Path(__file__).resolve().parent
+parent_path = current_dir.parent
+sys.path.append(os.path.join(parent_path, "users"))
+from get_profiles import get_filtered_profile, filter, filter_list
+
+csv_path = os.path.join("..", "users_data", "followers.csv")
+kumamoto_csv_path = os.path.join("..", "users_data", "kumamoto_followers.csv")
 
 #browser Settings
-driver = webdriver.Chrome(executable_path="C:\\Users\\xfura\\Desktop\\Instagram\\Instagram\\login\\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:\\Users\\xfura\\Desktop\\Instagram\\Instagram\\chromedriver.exe")
 #opne instagram
 driver.get("https://www.instagram.com")
+
+time.sleep(0.4)
+driver.maximize_window()
 
 #User settings
 load_dotenv()
@@ -39,18 +48,42 @@ profileButton.click()
 time.sleep(1.5)
 
 #move to followers
-followersButton = driver.find_element_by_css_selector(r"a[href='/bizco_careerup/followers/?next=%2F']")
-followersButton.click()
+# followersButton = driver.find_element_by_css_selector(r"a[href='/bizco_careerup/followers/?next=%2F']")
+# followersButton.click()
+pyautogui.moveTo(760, 230)
+pyautogui.click()
 
 time.sleep(1.5)
 
 #scroll and display all followers
 #last_height = driver.execute_script("return document.body.scrollHeight")
-for i in range(40):
-    driver.find_element_by_tag_name('body').click()
-    #driver.execute_script("window.scrollTo(0, %d)" % last_top)
-    driver.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
+pyautogui.moveTo(822, 586)
+pyautogui.click()
+refresher = 0
+long_html = False
+html_list = []
+while True:
+    if len(html_list) >= 3:
+        long_html = True
+    #driver.find_element_by_tag_name('body').click()
+    #driver.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
+    pyautogui.scroll(-1000)
     time.sleep(2)
+
+    if len(html_list) >= 3:
+        long_html = True
+    
+    if long_html:
+        html_list.pop(0)
+        if html_list[0] == html_list[1]:
+            refresher += 1
+            
+        else:
+            refresher = 0
+    
+    if refresher >= 3:
+        break
+    html_list.append(driver.page_source)
 
 
 #get followers list
@@ -62,6 +95,12 @@ for followers in follower_list:
 #as confirmation
 print(followers_list)
 
-with open("followers.csv1", "w", newline="") as f:
+kumamoto_followers_list = filter_list(followers_list)
+
+with open(csv_path, "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(followers_list)
+
+with open(kumamoto_csv_path, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(kumamoto_followers_list)
